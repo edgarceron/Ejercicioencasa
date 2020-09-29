9f32c71d7f2f4d788d2c2ab191ab8e292f234ab1
Edgar Mauricio Ceron Florez <edgar.ceron@correounivalle.edu.co>
2020-09-29 17:09:21 -0500

    Activities iniciales

---
+++ app/src/main/java/com/example/ejercicioencasa/GoalSelect.java
@@ -0,0 +1,14 @@
+package com.example.ejercicioencasa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GoalSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goal_select);
+    }
+}
