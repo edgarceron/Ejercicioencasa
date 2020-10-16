0e2c94ab4bc71bde1e16565455db1f180c842689
Edgar Mauricio Ceron Florez <edgar.ceron@correounivalle.edu.co>
2020-10-15 22:56:36 -0500

    Pruebas guardado base de datos

--- app/src/main/java/com/example/ejercicioencasa/GenderSelect.java
+++ app/src/main/java/com/example/ejercicioencasa/GenderSelect.java
@@ -2,16 +2,39 @@ package com.example.ejercicioencasa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.ejercicioencasa.daos.EjercicioDao;
+import com.example.ejercicioencasa.daos.InfoUsuario;
+import com.example.ejercicioencasa.daos.InfoUsuarioDao;
+
 public class GenderSelect extends AppCompatActivity {
 
+    public Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gender_select);
+        context = getApplicationContext();
+    }
+
+    public void alterInfoUsuario(String codigo, String valor){
+        InfoUsuario infoUsuario = new InfoUsuario(codigo,valor);
+        InfoUsuarioDao infoUsuarioDao = new InfoUsuarioDao(context);
+        infoUsuarioDao.alterInfoUsuario(infoUsuario);
+    }
+
+    public void btnMale(View view){
+        alterInfoUsuario(InfoUsuario.SEXO, "M");
+        this.btnNext(view);
+    }
+
+    public void btnFemale(View view){
+        alterInfoUsuario(InfoUsuario.SEXO, "F");
+        this.btnNext(view);
     }
 
     public void btnNext(View view){
--- app/src/main/java/com/example/ejercicioencasa/SplashScreen.java
+++ app/src/main/java/com/example/ejercicioencasa/SplashScreen.java
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -12,24 +13,57 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 
+import com.example.ejercicioencasa.daos.InfoUsuario;
+import com.example.ejercicioencasa.daos.InfoUsuarioDao;
+
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class SplashScreen extends AppCompatActivity {
+    public Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_splash_screen);
+        ActionBar actionBar = getSupportActionBar();
+       
+        assert actionBar != null;
+        actionBar.hide();
+
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        context = getApplicationContext();
+
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                Intent intent;
+                if(verifyUserData()){
+                    intent = new Intent(SplashScreen.this, MainActivity.class);
+                }
+                else {
+                    intent = new Intent(SplashScreen.this, GenderSelect.class);
+                }
                 startActivity(intent);
                 finish();
             }
         }, 3000);
+    }
+
+    public boolean verifyUserData(){
+        InfoUsuarioDao infoUsuarioDao = new InfoUsuarioDao(context);
+
+        String[] fields = new String[]{InfoUsuario.ALTURA, InfoUsuario.OBJETIVO, InfoUsuario.AMIGABLE,
+                InfoUsuario.PESO, InfoUsuario.SEXO};
+
+        for (String field : fields) {
+            if (infoUsuarioDao.consultarinfo(field) == null) {
+                return false;
+            }
+        }
+        return true;
     }
 }
