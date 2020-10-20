f3141b7d17dd36d4843ca9fa408e1294b11b7ca5
Jhon Carlos Valencia <carlosmcms725@hotmail.com>
2020-10-19 23:11:07 -0500

    Vistas actualizadas.

---
+++ app/src/main/java/com/example/ejercicioencasa/dificulty.java
@@ -0,0 +1,14 @@
+package com.example.ejercicioencasa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dificulty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dificulty);
+    }
+}
