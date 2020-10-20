353430cf5c58842db12ea9341153e197af54ede1
Edgar Mauricio Ceron Florez <edgar.ceron@correounivalle.edu.co>
2020-10-20 11:59:40 -0500

    Mostrando datos del usuario inicio

--- app/src/main/java/com/example/ejercicioencasa/UserInfo.java
+++ app/src/main/java/com/example/ejercicioencasa/UserInfo.java
@@ -2,13 +2,34 @@ package com.example.ejercicioencasa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.ejercicioencasa.daos.InfoUsuario;
+import com.example.ejercicioencasa.daos.InfoUsuarioDao;
 
 public class UserInfo extends AppCompatActivity {
 
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+        context = getApplicationContext();
+        InfoUsuarioDao infoUsuarioDao = new InfoUsuarioDao(context);
+        String sexo = infoUsuarioDao.consultarInfo(InfoUsuario.SEXO).getValor();
+
+        if(sexo.equals("M")) sexo = "Maculino";
+        else sexo = "Femenino";
+        TextView userSexo = findViewById(R.id.user_sexo);
+        userSexo.setText(sexo);
+
+        
+    }
+
+    public void cambiarInfoBtn(View view){
+        //Lanzar la actividad gender select *
     }
 }
